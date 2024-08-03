5887e872b17145096bd28c9401b781275904c8aa
Winston Ssentongo <Winston.Ssentongo@finca.org>
2024-08-03 13:51:21 +0300

    Minor change

--- helper_scripts/setup.py
+++ helper_scripts/setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
+
 setup(
     name = 'helper_scripts',
     packages = ['database_handler','send_mail','google_drive'],
@@ -8,6 +13,9 @@ setup(
     maintainer='Winston David Ssentongo',
     maintainer_email='winstondavid96@gmail.com',
     description='Database Helper Package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/winston1o1/helper-scripts',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
